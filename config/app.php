124d3b7da832260b62e98397f2cc022f054b3c30
BURUND1E <shesler2003@mail.ru>
2023-12-12 12:17:28 +0300

    3

--- config/app.php
+++ config/app.php
@@ -6,7 +6,16 @@ return [
     'identity' => \Model\User::class,
     //Классы для middleware
     'routeMiddleware' => [
-        'auth' => \Middleware\AuthMiddleware::class,
+        'auth' => \Middlewares\AuthMiddleware::class,
+    ],
+    'routeAppMiddleware' => [
+        'csrf' => \Middlewares\CSRFMiddleware::class,
+        'trim' => \Middlewares\TrimMiddleware::class,
+        'specialChars' => \Middlewares\SpecialCharsMiddleware::class,
+    ],
+    'validators' => [
+        'required' => \Validators\RequireValidator::class,
+        'unique' => \Validators\UniqueValidator::class
     ]
 ];
 
